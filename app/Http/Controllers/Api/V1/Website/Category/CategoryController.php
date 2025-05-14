4c95a025d6583d97a447f1714b69faa9a89b2233
ahmed <ahmedgamalfathy39@gmail.com>
2025-05-14 17:19:48 +0300

    edit productController

--- app/Http/Controllers/Api/V1/Website/Category/CategoryController.php
+++ app/Http/Controllers/Api/V1/Website/Category/CategoryController.php
@@ -15,6 +15,7 @@ class CategoryController extends Controller
 
    public function index(Request $request)  {
     $categories = Category::where('is_active',IsActive::ACTIVE)->get();
+    return $categories;
     return ApiResponse::success(new AllCategoryCollection(PaginateCollection::paginate($categories, $request->pageSize??10)));
    }
 }
