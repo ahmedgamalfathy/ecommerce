5f08ee69878dc04111daad8589a2620712cd8ecc
ahmed <ahmedgamalfathy39@gmail.com>
2025-05-25 12:24:39 +0300

    add commit to CreateProductRequest &UpdatetProductRequest

--- app/Http/Requests/Product/UpdateProductRequest.php
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -31,7 +31,7 @@ class UpdateProductRequest extends FormRequest
     {//categoryId, name, description, price, status
         return [
             "categoryIds" => ["required"],
-            "name" => ["required", "string", "max:255",Rule::unique('products')->ignore($this->route('product'))],
+            "name" => ["required", "string", "max:255",Rule::unique('products')->ignore($this->route('product'))],//ignore the product id
             "description" => ["nullable", "string", "max:255"],
             "price" => ["required"],
             "status" => ["required", new Enum(ProductStatus::class)],
