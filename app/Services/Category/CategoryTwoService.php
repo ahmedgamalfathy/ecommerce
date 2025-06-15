a139ba86681031fa18c5c0d8c66aae545a32a8c2
ahmed <ahmedgamalfathy39@gmail.com>
2025-06-15 17:58:49 +0300

    desc category

--- app/Services/Category/CategoryTwoService.php
+++ app/Services/Category/CategoryTwoService.php
@@ -28,6 +28,7 @@ class CategoryTwoService {
         ])
         ->mainCategories()
         ->with('SubCategories')
+        ->orderByDesc('id')
         ->get();
         return $categories;
     }
